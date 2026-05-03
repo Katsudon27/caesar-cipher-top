2abd3d3b90060a4d0c893a1569fa3248ecf6a723
Katsudon27 <katsudon1127@gmail.com>
2026-05-04 09:20:39 +1000

    Add optimisation for solution

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,41 +1,27 @@
-def caesar_cipher(string, key = 0, shift = "right")
-  # First, we split the provided string into an array with individual words
-  words = string.split
-
-  # Map is used here to return a new array of encrypted words
-  encrypted_string = words.map do |word|
-    # We then use split to turn each word into an array with individual letters
-    # and use reduce to create a new word with encrypted letters
-    word.chars.reduce("") do |encrypted_word, letter|
-      # Only apply encryption if the character is a letter
-      if letter.match?(/[A-Za-z]/)
-        # Different calculations for shifting and wrapping of letters
-        # depending on shift direction
-        case shift
-        when "right"
-          letter_code = letter.ord + key
-          if letter_code > 90 && letter_code < 97
-            letter_code = 64 + (letter_code - 90)
-          elsif letter_code > 122
-            letter_code = 96 + (letter_code - 122)
-          end
-        when "left"
-          letter_code = letter.ord - key
-          if letter_code < 65
-            letter_code = 91 - (65 - letter_code)
-          elsif letter_code < 97 && letter_code > 91
-            letter_code = 123 - (97 - letter_code)
-          end
-        end
-
-        # Convert the letter back to its string form
-        letter = letter_code.chr
-      end
-      # Append it to form the encrypted word
-      encrypted_word + letter
-    end
+def encrypt_letter(letter, key, is_upper_case, shift = "right")
+  normalise = is_upper_case ? "A" : "a"
+  case shift
+  when "right"
+    (((letter.ord - normalise.ord + key) % 26) + normalise.ord).chr
+  when "left"
+    (((letter.ord - normalise.ord - key) % 26) + normalise.ord).chr
   end
-  puts encrypted_string.join(" ")
 end
 
-caesar_cipher("What a string!", 5, "left")
+def caesar_cipher(string, key = 0, shift = "right")
+  string.split.map do |word|
+    word.chars.map do |letter|
+      # Only apply encryption if the character is a letter
+      case letter
+      when "a".."z"
+        encrypt_letter(letter, key, false, shift)
+      when "A".."Z"
+        encrypt_letter(letter, key, true, shift)
+      else
+        letter
+      end
+    end.join
+  end.join(" ")
+end
+
+puts caesar_cipher("What a string!", 5)
