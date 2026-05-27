4400f051b4271bdb5e4d2d7f03190154bd70f21f
Katsudon27 <katsudon1127@gmail.com>
2026-05-27 10:43:59 +1000

    Refactor methods to be inside a class

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,27 +1,31 @@
-def encrypt_letter(letter, key, is_upper_case, shift = "right")
-  normalise = is_upper_case ? "A" : "a"
-  case shift
-  when "right"
-    (((letter.ord - normalise.ord + key) % 26) + normalise.ord).chr
-  when "left"
-    (((letter.ord - normalise.ord - key) % 26) + normalise.ord).chr
+# Encrypts a phrase or word with Caesar Cipher
+class CaesarCipher
+  def encrypt_letter(letter, key, is_upper_case, shift = "right")
+    normalise = is_upper_case ? "A" : "a"
+    case shift
+    when "right"
+      (((letter.ord - normalise.ord + key) % 26) + normalise.ord).chr
+    when "left"
+      (((letter.ord - normalise.ord - key) % 26) + normalise.ord).chr
+    end
+  end
+
+  def encrypt_word(string, key = 0, shift = "right")
+    string.split.map do |word|
+      word.chars.map do |letter|
+        # Only apply encryption if the character is a letter
+        case letter
+        when "a".."z"
+          encrypt_letter(letter, key, false, shift)
+        when "A".."Z"
+          encrypt_letter(letter, key, true, shift)
+        else
+          letter
+        end
+      end.join
+    end.join(" ")
   end
 end
 
-def caesar_cipher(string, key = 0, shift = "right")
-  string.split.map do |word|
-    word.chars.map do |letter|
-      # Only apply encryption if the character is a letter
-      case letter
-      when "a".."z"
-        encrypt_letter(letter, key, false, shift)
-      when "A".."Z"
-        encrypt_letter(letter, key, true, shift)
-      else
-        letter
-      end
-    end.join
-  end.join(" ")
-end
-
-puts caesar_cipher("What a string!", 5)
+cipher = CaesarCipher.new
+puts cipher.encrypt_word("What a string!", 5)
